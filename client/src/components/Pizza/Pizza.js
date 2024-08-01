2496f0e3ec94c78a767e3b53148f5910efa61c12
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2024-08-02 00:18:35 +0530

    card css updated

--- client/src/components/Pizza/Pizza.js
+++ client/src/components/Pizza/Pizza.js
@@ -1,31 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Pizza.css'
 
-function Pizza({pizza}) {
-  return (
-    <>
-    <h1>{pizza.name}</h1>
-    <img src={pizza.image} alt='pizza-image' className='img-fluid pizza-img'/>
-    <div className='flex-container'>
-        <div className='w-100'>
-            <p>Varients</p>
-            <select>
-                {pizza.varients.map(varient=>{
-                    return <option value={varient}>{varient}</option>
-                })}
-            </select>
-        </div>
-        <div className='w-100'>
-            <p>Quantity</p>
-            <select>
-                {[...Array(10).keys()].map(( i)=>{
-                    return <option value={i+1}>{i+1}</option>
-                })}
-            </select>
-        </div>
-    </div>
-    </>
-  )
+function Pizza({ pizza }) {
+    const { quantity, setQuantity } = useState(1);
+    const { varient, setVarient } = useState('small');
+    return (
+        <>
+            <h1 className='pizza-name'>{pizza.name}</h1>
+            <img src={pizza.image} alt='pizza-image' className='img-fluid pizza-img' />
+            <div className='flex-container'>
+                <div>
+                    <p className='type-nm'>Varients</p>
+                    <select
+                        className='select-input'
+                        value={varient}
+                        onChange={(e) => { setVarient(e.target.value) }}
+                    >
+                        {pizza.varients.map(varient => {
+                            return <option value={varient}>{varient}</option>
+                        })}
+                    </select>
+                </div>
+                <div>
+                    <p  className='type-nm'>Quantity</p>
+                    <select
+                        className='select-input'
+                        value={quantity}
+                        onChange={(e) => { setQuantity(e.target.value) }}
+                    >
+                        {[...Array(10).keys()].map((i) => {
+                            return <option value={i + 1}>{i + 1}</option>
+                        })}
+                    </select>
+                </div>
+            </div>
+
+            <div className='flex-container'>
+                <div className='second-sub-div w-100'>
+                    <h1>Price : {pizza.prices[0][varient] * quantity}</h1>
+                </div>
+                <div className='second-sub-div w-100'>
+                    <button className='cart-button'>Add to Cart</button>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Pizza
--- client/src/data.js
+++ client/src/data.js
@@ -1,105 +1,105 @@
 const pizzas = [
     {
-        "name": "PEPPER BARBECUE CHICKEN",
-        "varients": [
+        name: "PEPPER BARBECUE CHICKEN",
+        varients: [
             "small",
             "medium",
             "large"
         ],
-        "prices": [
+        prices: [
             {
-                "small": 200,
-                "medium": 350,
-                "large": 400
-            }
+                small: 200,
+                medium: 350,
+                large: 400
+            },
         ],
-        "category": "nonveg",
-        "image": "http://1.bp.blogspot.com/-uIkWcp8lePs/VOuFDxHAuiI/AAAAAAAAcZw/4fSQ4w__mDA/s1600/BBQ+Chicken+Pineapple+Pizza+4.jpg"
+        category: "nonveg",
+        image: "http://1.bp.blogspot.com/-uIkWcp8lePs/VOuFDxHAuiI/AAAAAAAAcZw/4fSQ4w__mDA/s1600/BBQ+Chicken+Pineapple+Pizza+4.jpg"
     },
     {
-        "name": "Chicken Golden Delight",
-        "varients": [
+        name: "Chicken Golden Delight",
+        varients: [
             "small",
             "medium",
             "large"
         ],
-        "prices": [
+        prices: [
             {
-                "small": 200,
-                "medium": 350,
-                "large": 400
-            }
+                small: 200,
+                medium: 350,
+                large: 400
+            },
         ],
-        "category": "nonveg",
-        "image": "https://www.dominos.co.in/files/items/MicrosoftTeams-image_(14).png"
+        category: "nonveg",
+        image: "https://www.dominos.co.in/files/items/MicrosoftTeams-image_(14).png"
     },
     {
-        "name": "Non Veg Supreme",
-        "varients": [
-            "small",
+        name: "Non Veg Supreme",
+        varients: [
+           "small",
             "medium",
             "large"
         ],
-        "prices": [
+        prices: [
             {
-                "small": 200,
-                "medium": 350,
-                "large": 400
-            }
+                small: 200,
+                medium: 350,
+                large: 400
+            },
         ],
-        "category": "nonveg",
-        "image": "https://www.dominos.co.in/files/items/MicrosoftTeams-image_(13).png"
+        category: "nonveg",
+        image: "https://www.dominos.co.in/files/items/MicrosoftTeams-image_(13).png"
     },
     {
-        "name": "CHICKEN SAUSAGE",
-        "varients": [
+        name: "CHICKEN SAUSAGE",
+        varients: [
             "small",
             "medium",
             "large"
         ],
-        "prices": [
+        prices: [
             {
-                "small": 200,
-                "medium": 350,
-                "large": 400
-            }
+                small: 200,
+                medium: 350,
+                large: 400
+            },
         ],
-        "category": "nonveg",
-        "image": "https://www.dominos.co.in/files/items/MicrosoftTeams-image_(17).png"
+        category: "nonveg",
+        image: "https://www.dominos.co.in/files/items/MicrosoftTeams-image_(17).png"
     },
     {
-        "name": "The 4 Cheese Pizza",
-        "varients": [
-            "small",
+        name: "The 4 Cheese Pizza",
+        varients: [
+           "small",
             "medium",
             "large"
         ],
-        "prices": [
+        prices: [
             {
-                "small": 200,
-                "medium": 350,
-                "large": 400
-            }
+                small: 200,
+                medium: 350,
+                large: 400
+            },
         ],
-        "category": "veg",
-        "image": "https://www.dominos.co.in/files/items/PIZ0171.jpg"
+        category: "veg",
+        image: "https://www.dominos.co.in/files/items/PIZ0171.jpg"
     },
     {
-        "name": "Peppy Paneer",
-        "varients": [
-            "small",
+        name: "Peppy Paneer",
+        varients: [
+           "small",
             "medium",
             "large"
         ],
-        "prices": [
+        prices: [
             {
-                "small": 200,
-                "medium": 350,
-                "large": 400
-            }
+                small: 200,
+                medium: 350,
+                large: 400
+            },
         ],
-        "category": "veg",
-        "image": "https://www.dominos.co.in/files/items/Peppy_Paneer.jpg"
+        category: "veg",
+        image: "https://www.dominos.co.in/files/items/Peppy_Paneer.jpg"
     }
 ]
 
